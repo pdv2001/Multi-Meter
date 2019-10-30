3fd5f19dbdf27865b83a8314a049a13321d0b3de
pdv2001 <pdv_2004@yahoo.com>
2019-10-29 23:18:41 -0400

    jsonRainfall

--- daemon.sh
+++ daemon.sh
@@ -45,7 +45,7 @@ fi
 
 while true; do
   jsonRainfall=$(rtl_433 -F json -E Quit)
-  echo "Rain Gauge JSON output: $json"
+  echo "Rain Gauge JSON output: $jsonRainfall"
   
    rainfall=$(echo $jsonRainfall | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"Message\"][\"Consumption\"])/$cmToInches")
     
