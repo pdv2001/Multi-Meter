824c1d57057221e18f78508241955a863f00b382
pdv2001 <pdv_2004@yahoo.com>
2019-10-30 17:23:27 -0400

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -37,7 +37,8 @@ while true; do
   jsonRainfall=$(rtl_433 -F json -E quit)
   echo "Rain Gauge JSON output: $jsonRainfall"
   
-   rainfall=$(echo $jsonRainfall | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"Message\"][\"Consumption\"])/$cmToInches")
+  #rainfall=$(echo $jsonRainfall | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"Message\"][\"Consumption\"])/$cmToInches")
+  rainfall=$(echo $jsonRainfall | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"total_rain\"])/$cmToInches")
     
   # Only do something if a reading has been returned
   if [ ! -z "$rainfall" ]; then
