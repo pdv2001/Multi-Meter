0aef5ae85713b73876f36adcfb1cdc711cc68476
pdv2001 <pdv_2004@yahoo.com>
2019-10-31 08:07:36 -0400

    Forget conversion, try 2m00s, -v option

--- daemon.sh
+++ daemon.sh
@@ -33,22 +33,22 @@ fi
 
 while true; do
   #jsonOutput=$(rtl_433 -M RGR968 -E quit) #Rain gauge signal was very random
-  jsonOutput=$(rtl_433 -M RGR968 -T 60)
+  jsonOutput=$(rtl_433 -v -M RGR968 -T 2m00s)
   echo "Rain Gauge JSON output: $jsonOutput"
   rainfall_mm=$(echo $jsonOutput | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'rain_mm'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${1}p)
   #rainfall=$(echo $jsonOutput | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"total_rain\"])/$cmToInches")
   
   # Only do something if a reading has been returned
   if [ ! -z "$rainfall_mm" ]; then
-    rainfall_in=`echo "$rainfall_mm $mmToInches" | awk '{printf"%.2f \n", $1/$2}'`
+    #rainfall_in=`echo "$rainfall_mm $mmToInches" | awk '{printf"%.2f \n", $1/$2}'`
     rainrate_mm=$(echo $jsonOutput | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'rain_rate_mm_h'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${1}p)
-    rainrate_in=`echo "$rainrate_mm $mmToInches" | awk '{printf"%.2f \n", $1/$2}'`
-    echo "Total rain: $rainfall_in inches... Rate of fall: $rainrate_in inches/hr"
+    #rainrate_in=`echo "$rainrate_mm $mmToInches" | awk '{printf"%.2f \n", $1/$2}'`
+    echo "Total rain: $rainfall_mm mm... Rate of fall: $rainrate_mm mm/hr"
   else #Look for temperature
     temp_c=$(echo $jsonOutput | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'temperature_C'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${1}p)
     if [ ! -z "$temp_c" ]; then
-      temp_f=`echo "$temp_c" | awk '{printf"%.2f \n", $1*9/5+32}'`
-      echo "Temperature: $temp_f"
+      #temp_f=`echo "$temp_c" | awk '{printf"%.2f \n", $1*9/5+32}'`
+      echo "Temperature: $temp_c"
     else
       echo "***NO DATA***"
     fi
