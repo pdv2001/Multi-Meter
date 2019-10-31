dafcf9b9910e860ad5ea13ef5bcd93954273e4f1
pdv2001 <pdv_2004@yahoo.com>
2019-10-31 09:04:55 -0400

    Fix sed in awk and restore conversion

--- daemon.sh
+++ daemon.sh
@@ -20,15 +20,15 @@ while true; do
   
   # Only do something if a reading has been returned
   if [ ! -z "$rainfall_mm" ]; then
-    #rainfall_in=`echo "$rainfall_mm $mmToInches" | awk '{printf"%.2f \n", $1/$2}'`
-    rainrate_mm=$(echo $jsonOutput | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'rain_rate_mm_h'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${1}p)
-    #rainrate_in=`echo "$rainrate_mm $mmToInches" | awk '{printf"%.2f \n", $1/$2}'`
-    echo "Total rain: $rainfall_mm mm... Rate of fall: $rainrate_mm mm/hr"
+    rainfall_in=`echo "$rainfall_mm 25.4" | awk '{printf"%.2f \n", $1/$2}'`
+    rainrate_mm=$(echo $jsonOutput | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'rain_rate_mm_h'\042/){print $(i+1)}}}' | tr -d '"' | sed -n '1p')
+    rainrate_in=`echo "$rainrate_mm 25.4" | awk '{printf"%.2f \n", $1/$2}'`
+    echo "Total rain: $rainfall_in inches... Rate of fall: $rainrate_in in/hr"
   else #Look for temperature
-    temp_c=$(echo $jsonOutput | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'temperature_C'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${1}p)
+    temp_c=$(echo $jsonOutput | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'temperature_C'\042/){print $(i+1)}}}' | tr -d '"' | sed -n '1p')
     if [ ! -z "$temp_c" ]; then
-      #temp_f=`echo "$temp_c" | awk '{printf"%.2f \n", $1*9/5+32}'`
-      echo "Temperature: $temp_c"
+      temp_f=`echo "$temp_c" | awk '{printf"%.2f \n", $1*9/5+32}'`
+      echo "Temperature: $temp_f"
     else
       echo "***NO DATA***"
     fi
