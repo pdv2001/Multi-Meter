aa96a613b6e2cbe844d28870740cdb0a175a2165
pdv2001 <pdv_2004@yahoo.com>
2019-10-30 20:39:24 -0400

    readarray -t array
    readarray -t array < <(sed -n '/{/,/}/{s/[^:]*:[^"]*"\([^"]*\).*/\1/p;}' json2)

--- daemon.sh
+++ daemon.sh
@@ -39,8 +39,11 @@ while true; do
   jsonRainfall=$(rtl_433 -M RGR968 -E quit)
   echo "Rain Gauge JSON output: $jsonRainfall"
   #parsedOutput= JSON.parse($jsonRainfall)
-  rainfallTest=$(echo $jsonRainfall | python -c "import json, sys; [sys.stdout.write(x['rain_mm'] + '\n') for x in json.load(sys.stdin)]")
-  echo "rainfallTest: $rainfallTest"
+  #rainfallTest=$(echo $jsonRainfall | python -c "import json, sys; [sys.stdout.write(x['rain_mm'] + '\n') for x in json.load(sys.stdin)]")
+  #echo "rainfallTest: $rainfallTest"
+
+  readarray -t array < <(sed -n '/{/,/}/{s/[^:]*:[^"]*"\([^"]*\).*/\1/p;}' json2)
+  echo "Array: ${array[@]}"
   #rainfall=$(echo $jsonRainfall | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"Message\"][\"Consumption\"])/$cmToInches")
   rainfall=$(echo $jsonRainfall | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"total_rain\"])/$cmToInches")
   
