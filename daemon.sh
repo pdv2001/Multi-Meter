b921b4991256f05a35fac824e92fecd7b058f413
pdv2001 <pdv_2004@yahoo.com>
2019-10-30 20:52:10 -0400

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -42,7 +42,8 @@ while true; do
   #rainfallTest=$(echo $jsonRainfall | python -c "import json, sys; [sys.stdout.write(x['rain_mm'] + '\n') for x in json.load(sys.stdin)]")
   #echo "rainfallTest: $rainfallTest"
 
-  readarray -t array < <(sed -n '/{/,/}/{s/[^:]*:[^"]*"\([^"]*\).*/\1/p;}' jsonRainfall)
+  #readarray="$(echo "$jsonRainfall" | jq -r 'to_entries[] | "[" + (.key|@sh) + "]=" + (.value | @sh)'"
+  readarray -t array < <(sed -n '/{/,/}/{s/[^:]*:[^"]*"\([^"]*\).*/\1/p;}' $jsonRainfall)
   echo "Array: ${array[@]}"
   #rainfall=$(echo $jsonRainfall | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"Message\"][\"Consumption\"])/$cmToInches")
   rainfall=$(echo $jsonRainfall | python -c "import json,sys;obj=json.load(sys.stdin);print float(obj[\"total_rain\"])/$cmToInches")
