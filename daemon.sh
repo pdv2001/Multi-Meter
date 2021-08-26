e423d9d49a49757a79f5258d36f5d67c70fce473
pdv2001 <pdv_2004@yahoo.com>
2021-08-26 06:42:37 -0400

    Add duration to all meter scan

--- daemon.sh
+++ daemon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 8/26/21  - Add duration to all meter scan
 # 8/25/21  - Back to "-single=false"
 # 8/25/21  - Try "-single=true"
 # 8/25/21  - Allow scan of all nearby meters
@@ -138,7 +139,7 @@ while true; do
   if [ "$METER_SCAN" = "y" ]; then
     #Scan all nearby meters
     echo "Scanning all nearby meters"
-    json=$(rtlamr -msgtype=$MSG_TYPE -single=false -format=json)
+    json=$(rtlamr -msgtype=$MSG_TYPE -single=false -duration=300s -format=json)
     echo "Meters found: $json"
   fi
 
